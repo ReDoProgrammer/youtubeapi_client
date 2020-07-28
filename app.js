aeebdb1f948dccd525a83e974e8899b2099cde1f
ReDo <redoprogrammer@gmail.com>
2020-07-28 15:58:58 +0700

    set view engine: ejs. set static path: public,views

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 app = express();
-
+app.set('view engine','ejs');
+app.set('views',"./views");
+app.use(express.static("./public"));
 app.listen(5000,()=>{
   console.log('server is running on port: 5000');
+});
+
+app.get('/',function(req,res){
+  return res.render('home/index');  
 });
