76038613f311671f21b3d98909dd0bad2e59b428
ReDo <redoprogrammer@gmail.com>
2020-07-28 16:19:50 +0700

    config layout

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+expressLayout = require('express-ejs-layouts');
 app = express();
+
 app.set('view engine','ejs');
 app.set('views',"./views");
+app.use(expressLayout);
 app.use(express.static("./public"));
+expressLayout = require('express-ejs-layouts');
+
+app.set('layout', 'layouts/layout');
 app.listen(5000,()=>{
   console.log('server is running on port: 5000');
 });
 
 app.get('/',function(req,res){
-  return res.render('home/index');  
+  return res.render('home/index');
 });
